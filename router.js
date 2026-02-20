e67b38a169aafa26d76782e4716cd2990c2454dc
fakhzulraflis <rfakhzul@gmail.com>
2026-02-20 14:30:00 +0700

    INITIAL

---
+++ router.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function readHTMLFile(fileName) {
+  try {
+    const fileContent = fs.readFileSync(fileName);
+    return fileContent;
+  } catch (err) {
+    console.log(err.message);
+    return '';
+  }// get, post, put, delete.
+}
+
+const routes = {
+  '/': './views/index.html', //Pastikan route nya benar, tempat file nya
+  '/about': './views/about.html',
+  '/contact': './views/contact.html',
+  '/info': './views/info.html'
+};
+
+function handle(request, response) {
+  const url = request.url;
+  console.log(`Someone opens ${url}`);
+
+  response.setHeader('Content-Type', 'text/html');
+  if (!routes[url]) {
+    response.statusCode = 404;
+    response.end('<h1>Page not found</h1>');
+  } else {
+    response.statusCode = 200;
+    response.end(readHTMLFile(routes[url]));
+  }
+}
+
+module.exports = handle;
